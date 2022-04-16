15fa3cd9e16d691c7a96b15af64d3e57820309f6
L <you@example.com>
2022-04-16 15:52:35 -0400

    bruh

--- source/Main.hx
+++ source/Main.hx
@@ -20,7 +20,6 @@ class Main extends Sprite
 	var skipSplash:Bool = true; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
 	public static var fpsVar:FPS;
-	private static var dataPath:String = null;//the thing android uses	
 
 	// You can pretty much ignore everything from here on - your code should go in your states.
 
